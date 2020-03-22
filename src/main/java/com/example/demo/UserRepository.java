bec96658df01a37778c33e2caf3353bcc6f57bca
dslee <now4me@gmail.com>
2020-03-22 14:46:30 +0900

    초기 커밋

---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.demo.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
